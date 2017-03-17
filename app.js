7dffa0007a54da9cebb7e987fb47abc6cfdd4762
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-03-17 20:05:03 +0700

    Update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express()
-app.listen(80)
+app.listen(2000)
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', showHome)
+
 app.get('/status', showStatus)
 
 function showStatus(req, res) {
   res.send({status:'OK'})
+}
+
+function showHome(req, res) {
+  res.render('index.html')
 }
