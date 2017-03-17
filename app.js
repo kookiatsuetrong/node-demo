c17db5cf3662130e43dbc78495e1a83f6d43d715
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-03-17 20:17:59 +0700

    Update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 app.listen(2000)
 app.engine('html', require('ejs').renderFile)
-
 app.get('/', showHome)
-
 app.get('/status', showStatus)
+app.use( express.static('public') )
+app.use( showError )
 
 function showStatus(req, res) {
   res.send({status:'OK'})
@@ -13,4 +13,8 @@ function showStatus(req, res) {
 
 function showHome(req, res) {
   res.render('index.html')
+}
+
+function showError(req, res) {
+  res.render('error.html')
 }
