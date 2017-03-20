8509de83908f4072c805b529e4215e06246834c4
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-03-20 18:38:08 +0700

    Add register page

--- app.js
+++ app.js
@@ -3,36 +3,41 @@ var app = express()
 app.listen(2000)
 var mysql = require('mysql');
 var db = {
-  host:'128.199.119.79',
-  user:'imarket',
-  password:'p@ssword',
-  database:'imarket'
+	host:'128.199.119.79',
+	user:'imarket',
+	password:'p@ssword',
+	database:'imarket'
 }
 var pool = mysql.createPool(db)
 
 app.engine('html', require('ejs').renderFile)
 app.get('/', showHome)
 app.get('/list', showList);
+app.get('/register', showRegisterPage)
 app.get('/status', showStatus)
 app.use( express.static('public') )
 app.use( showError )
 
 function showStatus(req, res) {
-  res.send({status:'OK'})
+	res.send({status:'OK'})
 }
 
 function showHome(req, res) {
-  res.render('index.html')
+	res.render('index.html')
 }
 
 function showError(req, res) {
-  res.render('error.html')
+	res.render('error.html')
 }
 
 function showList(req, res) {
-  pool.query('select * from post',
-    function (error, data) {
-      res.render('list.html', {post: data})
-    }
-  )
+	pool.query('select * from post',
+		function (error, data) {
+			res.render('list.html', {post: data})
+		}
+	)
+}
+
+function showRegisterPage(req, res) {
+	res.render('register.html')
 }
